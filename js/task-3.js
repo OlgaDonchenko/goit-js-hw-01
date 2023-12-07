3d2d174382783513b33aca97e583f64e18542ada
OlgaDonchenko <n0931606045@gmail.com>
2023-12-07 15:04:14 +0200

    don`t work

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,12 @@
 function getElementWidth(content, padding, border) {
-    let contentWidth = parseFloat(content);
-    let paddingWidth = parseFloat(padding);
-    let borderWidth = parseFloat(border);
-    let totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
-
-    return totalWidth;
+    const contentValue = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+    const totalWidth = contentValue + (paddingValue + borderValue) * 2;
+    
+    return totalWidth
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(getElementWidth("50px", "8px", "4px")); // Поверне: 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // Поверне: 101
+console.log(getElementWidth("200px", "0px", "0px")); // Поверне: 200
